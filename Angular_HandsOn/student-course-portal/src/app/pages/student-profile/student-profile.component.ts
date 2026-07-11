c7dab409f266a5383c1acba596174811750960f7
mayanksinghrajpoot <mayanksinghrajpoot01@gmail.com>
2026-07-12 02:09:21 +0530

    feat(angular-hol1): complete environment setup, file description notes, and base components

---
+++ Angular_HandsOn/student-course-portal/src/app/pages/student-profile/student-profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-profile',
+  imports: [],
+  templateUrl: './student-profile.component.html',
+  styleUrl: './student-profile.component.css',
+})
+export class StudentProfile {}
