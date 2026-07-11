effbf9863db17df53c330c1d92b60e221a004dbc
mayanksinghrajpoot <mayanksinghrajpoot01@gmail.com>
2026-07-12 02:13:10 +0530

    feat(angular-hol4): build template-driven enrollment form with built-in validation and border styling

--- Angular_HandsOn/student-course-portal/src/app/app.routes.ts
+++ Angular_HandsOn/student-course-portal/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home.component';
 import { CourseList } from './pages/course-list/course-list.component';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form.component';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'courses', component: CourseList }
+  { path: 'courses', component: CourseList },
+  { path: 'enroll', component: EnrollmentForm }
 ];
+
 
