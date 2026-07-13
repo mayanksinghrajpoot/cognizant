21696e0df10b1f1bf3685e653a82c883e5f81d76
mayanksinghrajpoot <mayanksinghrajpoot01@gmail.com>
2026-07-13 13:29:45 +0530

    feat(microservices): implement api-gateway routing and LogFilter

---
+++ api-gateway/src/main/java/com/cognizant/gateway/filter/LogFilter.java
@@ -0,0 +1,29 @@
+package com.cognizant.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter, Ordered {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOGGER.info("START LogFilter: Incoming request targeting {}", exchange.getRequest().getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            LOGGER.info("END LogFilter: Request response sent back for {}", exchange.getRequest().getURI());
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // High priority filter
+    }
+}
